69a9deb76a657e6239014ca0286709eced2b37a9
majidakhoulani <majidakh1993@gmail.com>
2023-03-27 17:25:57 +0200

    mtan bootstrap project

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// var myNav = document.getElementById('#mynav');
+// window.onscroll = function () { 
+//     if (document.body.scrollTop >= 200 ) {
+//         myNav.classList.add("nav-colored");
+//         myNav.classList.remove("nav-transparent");
+//     } 
+//     else {
+//         myNav.classList.add("nav-transparent");
+//         myNav.classList.remove("nav-colored");
+//     }
+// };
+// var nav = document.querySelector('nav'); // Identify target
+
+// window.addEventListener('scroll', function(event) { // To listen for event
+//     event.preventDefault();
+
+//     if (window.scrollY <=400) { // Just an example
+//         nav.style.backgroundColor = '#212529'; // or default color
+//     } else {
+//         nav.style.backgroundColor = 'transparent';
+//     }
+// });
+// document.getElementById("myButton").onclick = function () {
+//     location.href = "/index.html";
+// };
